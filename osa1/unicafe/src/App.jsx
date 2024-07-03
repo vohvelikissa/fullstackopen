6f84e824095859b5cdea3162088597de78f5a53c
Pessi <--add>
2024-07-03 13:18:10 +0300

    broken 1.11

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,24 +1,41 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+	return (
+		<>
+			<button>{props.text}</button>
+		</>
+	)
+}
+
+const StatisticsLine = (props) => {
+	return (
+		<tr>
+			<td>{props.text}</td>
+			<td>{props.value}</td>
+		</tr>
+	)
+}
+
 const Statistics = (props) => {
-	if(props.all == 0) {
+	/*if(props.all == 0) {
 		return (
 			<div>
 				<h1>statistics</h1>
 				<p>No feedback given</p>
 			</div>
 		)
-	}
+	}*/
 	return (
-		<div>
+		<>
 	  		<h1>statistics</h1>
-	  		<p>good {props.good}</p>
-	  		<p>neutral {props.neutral}</p>
-	  		<p>bad {props.bad}</p>
-			<p>all {props.all}</p>
-			<p>average {props.average}</p>
-			<p>positive {props.positive}</p>
-		</div>
+			<StatisticsLine text="good" value={props.good} />
+			<StatisticsLine text="neutral" value={props.neutral} />
+			<StatisticsLine text="bad" value={props.bad} />
+			<StatisticsLine text="all" value={props.all} />
+			<StatisticsLine text="average" value={props.average} />
+			<StatisticsLine text="positive" value={props.positive} />
+		</>
 	)
 }
 
@@ -30,26 +47,11 @@ const App = () => {
 	const [average, setAverage] = useState(0)
 	const [positive, setPositive] = useState(0)
   	return (
-		<div>
+		<>
 	  		<h1>Give feedback</h1>
-	  		<button onClick={() => {
-				setGood(good + 1)
-				setAll(all + 1)
-				setAverage(((good * 1) + (bad * -1))/all)
-				setPositive((good/all)*100)
-			}}>good</button>
-	  		<button onClick={() => {
-				setNeutral(neutral + 1)
-				setAll(all + 1)
-				setAverage(((good * 1) + (bad * -1))/all)
-				setPositive((good/all)*100)
-			}}>neutral</button>
-	  		<button onClick={() => {
-				setBad(bad + 1)
-				setAll(all + 1)
-				setAverage(((good * 1) + (bad * -1))/all)
-				setPositive((good/all)*100)
-			}}>bad</button>
+			<Button text="good" setValue={setGood} value={good} />
+			<Button text="neutral" setValue={setNeutral} value={neutral} />
+			<Button text="bad" setValue={setBad} value={bad} />
 			<Statistics 
 				good={good}
 				bad={bad}
@@ -58,7 +60,7 @@ const App = () => {
 				average={average}
 				positive={positive}
 			/>
-	  	</div>
+	  	</>
   	)
 }
 
