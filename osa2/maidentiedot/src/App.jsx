14994e3cca67fa4a9e19b3ee00095034ba01bc79
Pessi <pessiv@outlook.com>
2025-03-03 17:02:23 +0200

    added looking up countries data

--- osa2/maidentiedot/src/App.jsx
+++ osa2/maidentiedot/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 
+import axios from 'axios'
+import { useEffect } from 'react';
+
 const SearchField = (props) => {
   return (
     <>
@@ -11,11 +14,65 @@ const SearchField = (props) => {
   )
 }
 
+const SearchResults = (props) => {
+  if (props.commonNames.length > 10) {
+    return (
+      <>
+      <p>Too many matches, specify another filter</p>
+      </> 
+    )
+  } else if (props.commonNames.length < 10 && props.commonNames.length > 1) {
+    return (
+      <>
+      {props.commonNames.map(country => <p key={country}>{country}</p>)}
+      </>
+    )
+  } else if (props.commonNames.length == 1) {
+    return (
+      <>
+      {props.commonNames.map(country => <h1 key={country}>{country}</h1>)}
+      <p>Capital: {props.countryData.capital[0]}</p>
+      </>
+    )
+  } else {
+    return (
+      <>
+      <p>No matches</p>
+      </>
+    )
+  }
+}
+
 function App() {
   const [searchTerm, setSearchTerm] = useState('Finland')
+  const [commonNames, setCommonNames] = useState([])
+  const [countryData, setCountryData] = useState({})
+  useEffect(() => {
+    axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+         .then((response) => {
+          const countries = []
+          response.data.forEach(country => {
+            if (country.name.common.toLowerCase().includes(searchTerm.toLowerCase())) {
+            countries.push(country.name.common)
+            setCommonNames(countries)
+            }
+          }
+        )
+      }
+    )
+    if(commonNames.length == 1) {
+      axios.get('https://studies.cs.helsinki.fi/restcountries/api/name/'+commonNames[0])
+        .then((response)=> {
+          setCountryData(response.data)
+          console.log(countryData)
+        }
+      )
+    }
+  })
   return (
     <>
     <SearchField searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+    <SearchResults commonNames={commonNames} countryData={countryData}/>
     </>
   )
 }
