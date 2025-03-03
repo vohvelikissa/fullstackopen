3ecf5fed8e3c9ffc959d0f655826283c595d667c
Pessi <pessiv@outlook.com>
2025-03-03 13:47:43 +0200

    initial version of the search field

--- osa2/maidentiedot/src/App.jsx
+++ osa2/maidentiedot/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 
-
-function App() {
-
+const SearchField = (props) => {
   return (
     <>
-      <h1>Country informer</h1>
+    <h1>Country Informer</h1>
+    <p>Search for country: <input value={props.searchTerm}/></p>
+    </>
+  )
+}
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState('Finland')
+  return (
+    <>
+    <SearchField searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
     </>
   )
 }
